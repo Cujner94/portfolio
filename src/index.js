c5a66a4cd9b30a84a002db5cb56884d1f1648a99
Cujner94 <nikolacujofoto@gmail.com>
2019-01-27 02:14:22 +0100

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-let one = 'hello'
+const navMain = document.querySelector('.nav-list');
+const sectionMain = document.querySelector('#main');
+let navArray = [...navMain.children];
+let sectionArray = [...sectionMain.children];
 
-console.log(one);
+navMain.addEventListener('click', (e) => {
+	if (e.target.tagName === "LI") {
+		let selector = e.target.innerText.toLowerCase();
+	
+		navArray.forEach((e) => {
+			if (e.classList.contains('nav-selected')) {
+				e.classList.remove('nav-selected');
+			}
+		});
+		e.target.classList.add('nav-selected');
+		
+		sectionArray.forEach((e) => {
+			if (e.classList.contains('main-selected')) {
+				e.classList.remove('main-selected');
+			}
+		})
+		
+		sectionArray.forEach((e) => {
+			if (e.id === selector) {
+				e.classList.add('main-selected');
+			}
+		})
+	}
+})
