e1b9d4a19612abbaeabb147374c23137d1e24f7e
Cujner94 <nikolacujofoto@gmail.com>
2019-02-03 17:33:44 +0100

    Added scroll and swipe

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ const navMain = document.querySelector('.nav-list');
 const sectionMain = document.querySelector('#main');
 let navArray = [...navMain.children];
 let sectionArray = [...sectionMain.children];
+let wheelTimeout;
+let currentEl;
+let currentNav;
+let navSuffix = '-nav';
 
+// Manage navigation
 navMain.addEventListener('click', (e) => {
 	if (e.target.tagName === "LI") {
+		
 		let selector = e.target.innerText.toLowerCase();
 	
 		navArray.forEach((e) => {
@@ -24,3 +30,85 @@ navMain.addEventListener('click', (e) => {
 		})	
 	}
 })
+
+// HANDLE SCROLLING IN BROWSER
+sectionMain.addEventListener('wheel', (e) => {
+	if (!wheelTimeout) {
+		wheelTimeout = setTimeout(() => {
+			wheelTimeout = null;
+			
+			currentEl = document.querySelector('.main-selected');
+			if (e.deltaY < 0 && currentEl.previousElementSibling) {
+				
+				currentNav = document.getElementById(currentEl.id+navSuffix);
+				
+				currentEl.classList.remove('main-selected');
+				currentNav.classList.remove('nav-selected');
+				currentEl.previousElementSibling.classList.add('main-selected');
+				currentNav.previousElementSibling.classList.add('nav-selected');
+			}
+			else if(e.deltaY > 0 && currentEl.nextElementSibling){
+				
+				currentNav = document.getElementById(currentEl.id+navSuffix);
+				
+				currentEl.classList.remove('main-selected');
+				currentNav.classList.remove('nav-selected');
+				currentEl.nextElementSibling.classList.add('main-selected');
+				currentNav.nextElementSibling.classList.add('nav-selected');
+			}
+		}, 100);
+	}
+});
+
+// HANDLE SWIPING ON MOBILE
+
+sectionMain.addEventListener('touchstart', handleTouchStart, false);        
+sectionMain.addEventListener('touchmove', handleTouchMove, false);
+
+let xDown = null;                                                        
+let yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+		const firstTouch = getTouches(evt)[0];   
+		currentEl = document.querySelector('.main-selected');                                   
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    let xUp = evt.touches[0].clientX;                                    
+    let yUp = evt.touches[0].clientY;
+
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+        if ( xDiff > 0 && currentEl.nextElementSibling) {
+					currentNav = document.getElementById(currentEl.id+navSuffix);
+				
+					currentEl.classList.remove('main-selected');
+					currentNav.classList.remove('nav-selected');
+					currentEl.nextElementSibling.classList.add('main-selected');
+					currentNav.nextElementSibling.classList.add('nav-selected');
+        } else if(xDiff < 0 && currentEl.previousElementSibling) {
+					currentNav = document.getElementById(currentEl.id+navSuffix);	
+				
+					currentEl.classList.remove('main-selected');
+					currentNav.classList.remove('nav-selected');
+					currentEl.previousElementSibling.classList.add('main-selected');
+					currentNav.previousElementSibling.classList.add('nav-selected');
+        }                       
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
